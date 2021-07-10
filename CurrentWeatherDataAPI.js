f1656791f5ef238c7074ce6a21ca78e284c8b958
Security-Development <35417717+Security-Development@users.noreply.github.com>
2021-07-11 03:49:35 +0900

    Update CurrentWeatherDataAPI.js

--- CurrentWeatherDataAPI.js
+++ CurrentWeatherDataAPI.js
@@ -6,6 +6,7 @@ request({
   url: apiURI,
   json: true
 }, (erro, response, JSON) => {
+  //JSON 데이터 확인 => console.log(JSON);
   const temp = Math.floor(JSON.main.temp / 10)+"°C";
   console.log(temp);
 });
